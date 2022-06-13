3f43c9e767931cbb0ee65eb6c2a63a94dafe9b9d
Eddie Endisch <endisch@itworksgroup.de>
2022-06-13 12:15:00 +0200

    refactoring

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -6,45 +6,19 @@ public class Game
     private List<int> Taken = new List<int>();
     public Player Player1, Player2;
     int _counter=0;
-
     public Game(Player player1,Player player2)
     {
         Player1 = player1;
         Player2 = player2;
         Turn();
     }
-
-    private void Winner(Player current)
-    {
-        current.Score++;
-        Console.WriteLine($"Congrats {current.Name} you won the game");
-        Console.WriteLine($"Current Score: {Player1.Name}: {Player1.Score}  {Player2.Name}:{Player2.Score}");
-        Console.WriteLine();
-    }
-
-    private bool Draw()
-    {
-        if (_counter > 7)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    private void DrawMessage()
-    {
-        Console.WriteLine("Its a draw!");
-        Console.WriteLine($"Current Score: {Player1.Name}:{Player1.Score} {Player2.Name}:{Player2.Score}");
-        Console.WriteLine();
-    }
-    
     private void Turn()
     {
         while (CheckWin() == false)
         {
-            if (Draw())
+            if (_counter>7)
             {
-                DrawMessage();
+                Draw();
                 break;
             }
             if (CheckWin() == false)
@@ -52,23 +26,24 @@ public class Game
                 Move(Player1);
                 if (CheckWin() == false)
                 {
+                    DrawBoard();
                     Move(Player2);
                 }
                 else if (CheckWin())
                 {
+                    DrawBoard();
                     Winner(Player1);
                 }
-            }
+            } 
             if (CheckWin())
             {
+                DrawBoard();                   
                 Winner(Player2);
             }
         }
     }
-
     private void Move(Player current)
     {
-        Console.Clear();
         DrawBoard(); 
         Console.WriteLine($"Ok {current.Name} your turn:");
         try
@@ -94,16 +69,28 @@ public class Game
             Move(current);
         }
     }
-
     private void DrawBoard()
     {
+        Console.Clear();
         Console.WriteLine("   {0}  |  {1}  |  {2}   ", Pos[1], Pos[2], Pos[3]);
         Console.WriteLine("-------------------");
         Console.WriteLine("   {0}  |  {1}  |  {2}   ", Pos[4], Pos[5], Pos[6]);
         Console.WriteLine("-------------------");
         Console.WriteLine("   {0}  |  {1}  |  {2}   ", Pos[7], Pos[8], Pos[9]);
     }
-
+    private void Winner(Player current)
+    {
+        current.Score++;
+        Console.WriteLine($"Congrats {current.Name} you won the game");
+        Console.WriteLine($"Current Score: {Player1.Name}: {Player1.Score}  {Player2.Name}:{Player2.Score}");
+        Console.WriteLine();
+    }
+    private void Draw()
+    {
+        Console.WriteLine("Its a draw!");
+        Console.WriteLine($"Current Score: {Player1.Name}:{Player1.Score} {Player2.Name}:{Player2.Score}");
+        Console.WriteLine();
+    }
     private bool CheckMove(int move)
     {
         foreach (int i in Taken)
@@ -120,7 +107,6 @@ public class Game
         }
         return true;
     }
-
     private bool CheckWin()
     {
         if (Pos[1]==Pos[2] && Pos[2]==Pos[3] ||
